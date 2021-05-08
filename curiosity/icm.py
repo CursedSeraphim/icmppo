d214a7b79c26c99e38386a45aa692ced63b502c4
cursedseraphim <cat-noir@gmx.at>
2021-05-08 16:05:29 +0200

    "recurrent", updates for batch predictions

--- curiosity/icm.py
+++ curiosity/icm.py
@@ -99,7 +99,8 @@ class CNNICMModel(ICMModel):
 
     @property
     def recurrent(self) -> bool:
-        return False
+        # to enable batch-wise reward prediction
+        return True
 
     def forward(self, state: Tensor, next_state: Tensor, action: Tensor):
         state = self.encoder(state)
@@ -196,10 +197,6 @@ class ICM(Curiosity):
         return self.model.parameters()
 
     def reward(self, rewards: np.ndarray, states: np.ndarray, actions: np.ndarray) -> np.ndarray:
-        if len(actions.shape) == 1:
-            actions = actions.reshape(1, -1)
-        if len(rewards.shape) == 1:
-            rewards = rewards.reshape(1, -1)
         
         n, t = actions.shape[0], actions.shape[1]
         states, next_states = states[:-1], states[1:]
@@ -207,27 +204,13 @@ class ICM(Curiosity):
             self.state_converter.reshape_as_input(states, self.model.recurrent),
             self.state_converter.reshape_as_input(next_states, self.model.recurrent),
             actions.reshape(n * t, *actions.shape[2:]))
-        if len(actions.shape) == 1:
-            actions = actions.reshape(1, -1)
-        if len(states.shape) == 3:
-            states = states.unsqueeze(0)
-        if len(next_states.shape) == 3:
-            next_states = next_states.unsqueeze(0)
         states = states.float()
         next_states = next_states.float()
         next_states_latent, next_states_hat, _ = self.model(states, next_states, actions)
         intrinsic_reward = self.reward_scale / 2 * (next_states_hat - next_states_latent).norm(2, dim=-1).pow(2)
         intrinsic_reward = intrinsic_reward.cpu().detach().numpy().reshape(n, t)
-        # self.reporter.scalar('icm/reward',
-        #                      intrinsic_reward.mean().item() if self.reporter.will_report('icm/reward') else 0)
-
-        # self.reporter.scalar('icm/reward_extrinsic',
-        #                      rewards.mean().item() if self.reporter.will_report('icm/reward_extrinsic') else 0)
 
         combined_reward = (1. - self.intrinsic_reward_integration) * rewards + self.intrinsic_reward_integration * intrinsic_reward
-        # self.reporter.scalar('icm/reward_intrinsic_and_extrinsic',
-        #                      combined_reward.mean().item() if self.reporter.will_report('icm/reward_intrinsic_and_extrinsic') else 0)
-        # return combined rewards, intrinsic rewards, extrinsic rewards
         return combined_reward, intrinsic_reward, rewards
 
     def loss(self, policy_loss: Tensor, states: Tensor, next_states: Tensor, actions: Tensor) -> Tensor:
@@ -235,10 +218,6 @@ class ICM(Curiosity):
         forward_loss = 0.5 * (next_states_hat - next_states_latent.detach()).norm(2, dim=-1).pow(2).mean()
         inverse_loss = self.action_converter.distance(actions_hat, actions)
         curiosity_loss = self.weight * forward_loss + (1 - self.weight) * inverse_loss
-        # self.reporter.scalar('icm/loss_policy', policy_loss.item())
-        # self.reporter.scalar('icm/loss_forward', forward_loss.item())
-        # self.reporter.scalar('icm/loss_inverse', inverse_loss.item())
-        # self.reporter.scalar('icm/loss', curiosity_loss.item())
         return self.policy_weight * policy_loss + curiosity_loss, policy_loss, forward_loss, inverse_loss, curiosity_loss
 
     def to(self, device: torch.device, dtype: torch.dtype):
